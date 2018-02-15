1291d8ea1c426c573208d6c8a8261477e8a526ae
Michael Bull <mikebull94@gmail.com>
2018-02-15 23:41:48 +0000

    Move test data out of main sourceset

--- src/test/kotlin/com/github/michaelbull/quadtree/QuadTreeTest.kt
+++ src/test/kotlin/com/github/michaelbull/quadtree/QuadTreeTest.kt
@@ -7,12 +7,6 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 
 class QuadTreeTest {
-    private val comparator = Comparator<Point> { (x1, y1), (x2, y2) ->
-        val v1 = Math.sqrt(((x1 * x1) + (y1 * y1)).toDouble()).toFloat()
-        val v2 = Math.sqrt(((x2 * x2) + (y2 * y2)).toDouble()).toFloat()
-        v1.compareTo(v2)
-    }
-
     @Test
     fun intersect() {
         val tree = QuadTree(BoundingBox(
@@ -20,14 +14,34 @@ class QuadTreeTest {
             topRight = Point(1F, 1F)
         ))
 
-        DataSet.read("dataset").forEach(tree::insert)
+        readDataSet("dataset").forEach(tree::insert)
 
-        val expected = DataSet.read("expected")
+        val expected = readDataSet("expected")
         val actual = tree.intersect(BoundingBox(
             bottomLeft = Point(-0.1F, -0.1F),
             topRight = Point(0.1F, 0.1F)
         )).sortedWith(comparator)
 
         assertThat(actual, equalTo(expected))
+    }
+
+    private fun readDataSet(name: String): List<Point> {
+        return javaClass.classLoader
+            .getResourceAsStream("$name.txt")
+            .bufferedReader()
+            .useLines { it.map(::lineToPoint).toList() }
+    }
+
+    private fun lineToPoint(line: String): Point {
+        val split = line.split(",")
+        val x = split[0].toFloat()
+        val y = split[1].toFloat()
+        return Point(x, y)
+    }
+
+    private val comparator = Comparator<Point> { (x1, y1), (x2, y2) ->
+        val v1 = Math.sqrt(((x1 * x1) + (y1 * y1)).toDouble()).toFloat()
+        val v2 = Math.sqrt(((x2 * x2) + (y2 * y2)).toDouble()).toFloat()
+        v1.compareTo(v2)
     }
 }
